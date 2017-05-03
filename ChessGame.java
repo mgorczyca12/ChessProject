d2272144eed980b82e065e09a0247d046c106617
ghli22 <ghli@stu.naperville203.org>
2017-05-03 15:10:30 -0500

    added things

--- ChessGame.java
+++ ChessGame.java
@@ -1,7 +1,46 @@
-public class ChessGame{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
+public class ChessGame extends JFrame implements Runnable{
+
+	MouseListener menuListener;
+	JLayeredPane mainPanel;
+
+	public ChessGame(){super();}
+
+	public void run(){
+
+		this.setSize(700,700);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Chess Game");
+		this.setLayout(null);
+
+		mainPanel = new JLayeredPane();
+		mainPanel.setBounds(0,0,700,700);
+		mainPanel.setLayout(null);
+
+		this.add(mainPanel);
+		this.setVisible(true);
+
+		mainPanel.add(new JComponent(){
+			{this.setBounds(100,1
+			00,100,100);}
+			public void paintComponent(Graphics g){
+				g.setColor(new Color(120,244,12));
+				g.fillRect(0,0,100,100);
+			}}, 1);
+
+	}
     public static void main(String []args)
     {
-        System.out.println("Hello World");
+        SwingUtilities.invokeLater(new ChessGame());
     }
+}
+class Menu extends JPanel {
+	java.util.List<JButton> buttons = new ArrayList<>();
+	public void add(JButton b){
+		this.add(b);
+	}
 }
