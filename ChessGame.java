cd56f716b3ec8fec4955b6a5e3e51e86fff4005f
ghli22 <ghli@stu.naperville203.org>
2017-05-05 12:00:32 -0500

    things

--- ChessGame.java
+++ ChessGame.java
@@ -23,15 +23,31 @@ public class ChessGame extends JFrame implements Runnable{
 
 		this.add(mainPanel);
 		this.setVisible(true);
-
+		//test things
+		/*
 		mainPanel.add(new JComponent(){
 			{this.setBounds(100,100,100,100);}
 			public void paintComponent(Graphics g){
 				g.setColor(new Color(120,244,12));
 				g.fillRect(0,0,100,100);
 			}}, 1);
-
+		mainPanel.add(new JComponent(){
+			{this.setBounds(200,200,100,100);}
+			public void paintComponent(Graphics g){
+				g.setColor(new Color(120,244,12));
+				g.fillRect(0,0,100,100);
+			}}, 2);
 		mainPanel.remove(mainPanel.getComponents()[0]);
+		//*/
+		Menu menu = new Menu();
+		menu.setBounds(0,0,700,700);
+		JButton tButton = new JButton("Play");
+		JButton tButton1 = new JButton("Credits");
+		JButton tButton2 = new JButton("Quit");
+		menu.add(tButton);
+		menu.add(tButton1);
+		menu.add(tButton2);
+		mainPanel.add(menu,1);
 
 	}
     public static void main(String []args)
@@ -40,13 +56,18 @@ public class ChessGame extends JFrame implements Runnable{
     }
 }
 class Menu extends JPanel {
+	GridBagConstraints gbc;
 	java.util.List<JButton> buttons = new ArrayList<>();
 	public void add(JButton b){
 		buttons.add(b);
-		this.add(b);
+		super.add(b, gbc);
 	}
 	public Menu(){
 		super();
-		this.setLayout(new BoxLayout(this,BoxLayout.LINE_AXIS));
+		this.setLayout(new GridBagLayout());
+		gbc = new GridBagConstraints();
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+
 	}
 }
