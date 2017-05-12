b6991df68a7ac99b5f98d0c58f1c1b07282086ac
mgorczyca12 <MGorczyca@student.naperville203.org>
2017-05-12 15:10:34 -0500

    fuck my life

--- Rook.java
+++ Rook.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Rook extends Piece{
 
+
+
 	public Rook(char side, int row, int col, String name)
 	{
 		super(side, row, col, name);
@@ -9,11 +11,16 @@ public class Rook extends Piece{
 
 	public List<coordinate> possibleMoves(ChessBoard b)
 	{
+		int i = 1;
 		ArrayList<coordinate> moves = new ArrayList<>();
 
+		while(b.getSpot(super.getRow + i, super.getCol).checkAvailability() != false || b.getSpot(super.getRow + i, super.getCol).)
+		{
+			if(b.getSpot(super.getRow + i, super.getCol).getRow() >= 0)
+			{
 
-
-		while(b.getSpot(super.getRow() + v, super.getCol().getRow() >= 0 && b.getSpot(super.getRow() + v, super.getCol().getCol() >= 0)
+			}
+		}
 
 
 		return moves;
