0f44d0de908d171be1058521b05809e10312cff4
mgorczyca12 <mgorczyca@stu.naperville203.org>
2017-05-13 18:33:51 -0500

    did stuff

--- Bishop.java
+++ Bishop.java
@@ -1,2 +1,60 @@
 public class Bishop extends Piece{
+
+	public Bishop(char side, int row, int col, String name)
+	{
+		super(side, row, col, name);
+	}
+
+	public List<coordinate> possibleMoves(ChessBoard b)
+	{
+		int i = 1;
+		ArrayList<coordinate> moves = new ArrayList<>();
+
+		while(b.getSpot(super.getRow + i, super.getCol + i).checkAvailability() != false || b.getSpot(super.getRow + i, super.getCol + i).getSide() != super.getSide() || i > 8)
+		{
+			if(b.getSpot(super.getRow + i, super.getCol + i).getRow() >= 0)
+			{
+				moves.add(new coordinate(super.getRow + i, super.getCol + i));
+			}
+			i++;
+		}
+
+		i = 1;
+
+		while(b.getSpot(super.getRow - i, super.getCol - i).checkAvailability() != false || b.getSpot(super.getRow - i, super.getCol - i).getSide() != super.getSide() || i > 8)
+		{
+			if(b.getSpot(super.getRow - i, super.getCol - i).getRow() >= 0)
+			{
+				moves.add(new coordinate(super.getRow - i, super.getCol - i));
+			}
+			i++;
+		}
+
+		i = 1;
+
+		while(b.getSpot(super.getRow - i, super.getCol + i).checkAvailability() != false || b.getSpot(super.getRow - i, super.getCol + i).getSide() != super.getSide() || i > 8)
+		{
+			if(b.getSpot(super.getRow - i, super.getCol + i).getRow() >= 0)
+			{
+				moves.add(new coordinate(super.getRow - i, super.getCol + i));
+			}
+			i++;
+		}
+
+		i = 1;
+
+		while(b.getSpot(super.getRow + i, super.getCol - i).checkAvailability() != false || b.getSpot(super.getRow + i, super.getCol - i).getSide() != super.getSide() || i > 8)
+		{
+			if(b.getSpot(super.getRow + i, super.getCol - i).getRow() >= 0)
+			{
+				moves.add(new coordinate(super.getRow + i, super.getCol - i));
+			}
+			i++;
+		}
+
+
+		return moves;
+
+	}
+
 }
--- Rook.java
+++ Rook.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 public class Rook extends Piece{
 
-
-
 	public Rook(char side, int row, int col, String name)
 	{
 		super(side, row, col, name);
@@ -14,12 +12,46 @@ public class Rook extends Piece{
 		int i = 1;
 		ArrayList<coordinate> moves = new ArrayList<>();
 
-		while(b.getSpot(super.getRow + i, super.getCol).checkAvailability() != false || b.getSpot(super.getRow + i, super.getCol).)
+		while(b.getSpot(super.getRow + i, super.getCol).checkAvailability() != false || b.getSpot(super.getRow + i, super.getCol).getSide() != super.getSide() || i > 8)
 		{
 			if(b.getSpot(super.getRow + i, super.getCol).getRow() >= 0)
 			{
-
+				moves.add(new coordinate(super.getRow + i, super.getCol));
 			}
+			i++;
+		}
+
+		i = 1;
+
+		while(b.getSpot(super.getRow - i, super.getCol).checkAvailability() != false || b.getSpot(super.getRow - i, super.getCol).getSide() != super.getSide() || i > 8)
+		{
+			if(b.getSpot(super.getRow - i, super.getCol).getRow() >= 0)
+			{
+				moves.add(new coordinate(super.getRow - i, super.getCol));
+			}
+			i++;
+		}
+
+		i = 1;
+
+		while(b.getSpot(super.getRow, super.getCol + i).checkAvailability() != false || b.getSpot(super.getRow, super.getCol + i).getSide() != super.getSide() || i > 8)
+		{
+			if(b.getSpot(super.getRow, super.getCol + i).getRow() >= 0)
+			{
+				moves.add(new coordinate(super.getRow, super.getCol + i));
+			}
+			i++;
+		}
+
+		i = 1;
+
+		while(b.getSpot(super.getRow, super.getCol - i).checkAvailability() != false || b.getSpot(super.getRow, super.getCol - i).getSide() != super.getSide() || i > 8)
+		{
+			if(b.getSpot(super.getRow, super.getCol - i).getRow() >= 0)
+			{
+				moves.add(new coordinate(super.getRow, super.getCol - i));
+			}
+			i++;
 		}
 
 
